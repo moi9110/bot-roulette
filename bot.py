715d78215bf73141a9cb02a09738ff3efbb45f3c
moi9110 <nustimada@gmail.com>
2025-08-09 05:02:31 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
+import os
 import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # Permet d'accéder au contenu des messages
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -14,4 +15,10 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("Pong!")
 
-bot.run("MTQwMTAxOTY0NDc5MzMyNzY3OA.GsQY83.up-DWlA787FUtPkXzSpCJN18J8rXTCK3ErSRB0")
+# Récupère le token depuis la variable d'environnement
+token = os.getenv("MTQwMTAxOTY0NDc5MzMyNzY3OA.GI4WpR.2JWRwuRM8m3MrDjZlH1FZfNMzKTUpyLhDyPW3k")
+
+if token is None:
+    print("Erreur : Le token Discord n'est pas défini dans la variable d'environnement 'TOKEN'.")
+else:
+    bot.run(token)
